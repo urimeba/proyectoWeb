81ed31189ee96ad7a0d448ef2b58f48ed3488b1a
urimeba <urimeba511@gmail.com>
2019-06-04 12:11:46 -0500

    *changed* Usuario: views: registrar, inicio

--- proyecto/Apps/Usuarios/views.py
+++ proyecto/Apps/Usuarios/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
+from Apps.Usuarios import models as models_usuarios
+from Apps.Colonias import models as models_colonias
 
 # Create your views here.
 
@@ -13,7 +15,7 @@ def registrar_usuario(request):
     correo = request.POST.get('correo')
 
     # VERIFICANDO QUE EXISTA UN USUARIO CON ESE CORREO.
-    # SI EXISTE, SE VA HACIA EL EXCEPTION
+    # SI NO EXISTE, SE VA HACIA EL EXCEPTION
     try:
         usuario_correo = User.objects.get(email=correo)
         return HttpResponse("Error: Ese correo ya esta tomado")
@@ -22,10 +24,18 @@ def registrar_usuario(request):
 
         # INTENTAMOS CREAR EL USUARIO
         try:
+            # AGREGANDO EL USUARIO A USER.AUTH
             usuario = User.objects.create_user(usuario, correo, contraseña)
             usuario.first_name = nombre
-            usuario.las_name = apellido
+            usuario.last_name = apellido
             usuario.save()
+
+            # OBTENEMOS EL ID DE LA COLONIA ELEGIDA Y EL ID DEL USUARIO CREADO AQUI ARRIBA
+            id_colonia = models_colonias.Colonias.get(nombre=colonia).id
+            id_usuario = usuario.id
+            
+            # AGREGANDO EL USUARIO A USUARIOS_USUARIOS
+            usuario_usuario = models_usuarios.Usuarios(colonia_id=id_colonia, usuario_id=id_usuario)
             return HttpResponse("Registro exitoso")
         except Exception as e:
             print(e)
@@ -41,6 +51,10 @@ def iniciarSesion(request):
     if user is not None:
         if user.is_active:
             login(request, user)
+
+            id_colonia = models_usuarios.Usuarios.get(id=user.id).colonia_id
+            request.session['colonia'] = id_colonia
+            
             return HttpResponse("Inicio de sesion correcto")
         else:
             return HttpResponse("Error: datos incorrectos")
