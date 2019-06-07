20e2b98cbd6b9910522ad913f3838292658adf1c
urimeba <urimeba511@gmail.com>
2019-06-07 12:59:16 -0500

    Auto stash before merge of "master" and "origin/master"
    *ignored* .git, .pyc

--- proyecto/Apps/Categorias/urls.py
+++ proyecto/Apps/Categorias/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
 
     # LAS APLICACIONES SE ENCUENTRAN COMENTADAS
     # SI LAS OCUPAS, DESCOMENTALAS
