c07d72dc3ef02beaabbc348a9aa332a820a9c131
urimeba <urimeba511@gmail.com>
2019-06-07 20:34:33 -0500

    *changed* iniciarSesion, registrarUsuario, views_categorias

--- proyecto/Apps/Categorias/views.py
+++ proyecto/Apps/Categorias/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from Apps.Categorias import models as models_categorias
 
 # Create your views here.
+def index(request):
+    categorias = models_categorias.Categorias.objects.all()
+    respuesta = []
+
+    for categoria in categorias:
+        respuesta.append(categoria.nombre)
+
+    print(respuesta)
+    return render(request, "base.html", {'categorias':respuesta})
--- proyecto/Apps/Publicaciones/views.py
+++ proyecto/Apps/Publicaciones/views.py
@@ -6,14 +6,4 @@ from Apps.Categorias import models as models_categorias
 import json
 
 # Create your views here.
-def index(request):
-    categorias = models_categorias.Categorias.objects.all()
-    respuesta = []
 
-    for categoria in categorias:
-        respuesta.append(categoria.nombre)
-
-
-
-    
-    return render(request, "base.html", {'categorias':respuesta})
