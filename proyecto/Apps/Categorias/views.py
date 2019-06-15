0f5d770512f98be84a2eacbe728174537114aa90
urimeba <urimeba511@gmail.com>
2019-06-14 20:18:13 -0500

    *added* Form Publicaciones

--- proyecto/Apps/Categorias/views.py
+++ proyecto/Apps/Categorias/views.py
@@ -4,9 +4,11 @@ from Apps.Publicaciones import models as models_publicaciones
 from Apps.Colonias import models as models_colonias
 from django.db.models import Count
 from django.contrib.auth.models import User
+from Apps.Publicaciones import forms
 
 # Create your views here.
 def obtenerCategorias(request):
+    
     if 'colonia' in request.session:
         publicaciones = []
         categorias = models_categorias.Categorias.objects.all()
@@ -18,7 +20,19 @@ def obtenerCategorias(request):
             except Exception as e:
                 print(e)
 
-        return render(request, "base.html", {'categorias':categorias,'publicaciones':publicaciones})
+        if request.method=='POST':
+            form = forms.PublicacionFormulario(request.POST)
+            if form.is_valid():
+                titulo = request.POST.get('titulo')
+                descripcion = request.POST.get('descripcion')
+                categoria = request.POST.get('categoria')
+                models_publicaciones.Publicaciones.create(titulo=titulo,descripcion=descripcion,categoria_id=categoria,colonia_id=request.session['colonia'],usuario_id=request.user.id)
+            else:
+                print("EL FORMULARIO ESTA MAL")
+            return redirect('obtenerCategorias')
+        else:
+            form = forms.PublicacionFormulario()
+            return render(request, "base.html", {'categorias':categorias,'publicaciones':publicaciones,'form':form})
     else:
         return redirect('/')
 
--- proyecto/Apps/Publicaciones/forms.py
+++ proyecto/Apps/Publicaciones/forms.py
@@ -4,7 +4,7 @@ from Apps.Publicaciones import models as models_publicaciones
 
 class PublicacionFormulario(forms.Form):
     cate = models_categorias.Categorias.objects.all()
-    CHOICES = tuple((o.pk, o.nombreCategoria) for o in cate) 
+    CHOICES = tuple((o.pk, o.titulo) for o in cate) 
        
     titulo =   forms.CharField(widget=forms.TextInput(attrs={'class':'form__input', 'placeholder':'Titulo'}))
     descripcion =   forms.CharField(widget=forms.TextInput(attrs={'class':'form__input', 'placeholder':'Descripcion'}))
